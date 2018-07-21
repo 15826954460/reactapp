5b4517d62ba549b6198fad1911f24532a0ae8436
15826954460 <624793604@qq,com>
2018-07-21 21:16:21 +0800

    [代码分割的配置模板]

--- src/pages/inbox.js
+++ src/pages/inbox.js
@@ -7,35 +7,10 @@ export default class Inbox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null,
-      isLoading: true,
-      isError: false,
     };
   }
 
   componentDidMount() {
-    Loading({
-      isLoading: this.state.isLoading,
-      isError: this.state.isError,
-    })
-    WebAPI.topics.topic('topics').then((res) => {
-      console.log(res.data)
-      // cnode 返回的是 boolean: true
-      if (res.success) {
-        this.setState({
-          ...this.state,
-          data: res.data,
-          isLoading: false,
-          isError: false,
-        })
-      } else {
-        Loading({
-          ...this.state,
-          isLoading: false,
-          isError: true,
-        })
-      }
-    })
   }
 
   componentWillMount() {
@@ -53,7 +28,6 @@ export default class Inbox extends Component {
     return (
       <div>
         <h3>{"Inbox"}</h3>
-        {this.state.data ? this.state.data[0].id : null}
       </div>
     );
   }
